56928007f2acfff75ad582e81e8c649a14626253
Theo Richart <theor@unity3d.com>
2021-09-06 19:49:08 -0400

    fix test

--- test/test-generic-post.js
+++ test/test-generic-post.js
@@ -56,7 +56,7 @@ describe("check build output for a generic post", () => {
     });
 
     it("should have inlined css", () => {
-      const css = select("style");
+      const css = select("style:not([data-href])");
       expect(css).to.match(/header nav/);
       expect(css).to.not.match(/test-dead-code-elimination-sentinel/);
     });
